314be8d734daeadebe0f7da55a168a9461121690
Fahim-M-Siam <fahim.mohammad.siam@gmail.com>
2026-04-01 23:00:00 +0600

    Updated all the files

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/app
+cd /home/ubuntu/app
 pm2 start app.js --name "backend-app"
 pm2 save
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# Stop existing app if running
 pm2 stop all || true
 pm2 delete all || true
-
-# Clean old files
-rm -rf /home/ec2-user/app
-mkdir -p /home/ec2-user/app
+rm -rf /home/ubuntu/app
+mkdir -p /home/ubuntu/app
